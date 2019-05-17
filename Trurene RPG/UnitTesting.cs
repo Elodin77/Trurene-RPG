f691f78cbc14c940fab1b5bf4d68d88dab7c6e51
Djimon Jayasundera <1015097@student.ccgs.wa.edu.au>
2019-05-17 10:34:50 +0800

    Formatted testing file

--- Trurene RPG/UnitTesting.cs
+++ Trurene RPG/UnitTesting.cs
@@ -1,5 +1,6 @@
-﻿/* This file contains all of the test cases for testing the functions
- * in the Program file.
+﻿/* This file contains all of the test cases for testing some of the functions
+ * in the Program file. These functions are the ones which may not have an obvious
+ * effect on the gameplay and therefore bugs in them are hard to identify.
  */
 using System.Diagnostics;
 using static Trurene_RPG.Program;
@@ -10,9 +11,10 @@ namespace Trurene_RPG
     {
         public static void RunUnitTesting() {
             // Run tests to ensure the validity of following unit tests
+            // i.e. If this test fails, then the other tests may incorrectly identify bugs.
             Debug.Assert(DistanceBetween(world.aurora.pos, world.trollKing.pos) > 0);
 
-            // DistanceBetween
+            // Test DistanceBetween
             Position pos1 = new Position();
             Position pos2 = new Position();
             pos1.row = 1;
@@ -22,19 +24,19 @@ namespace Trurene_RPG
             Debug.Assert(DistanceBetween(pos1, pos2) == 8);
             Debug.Assert(DistanceBetween(pos2, pos1) == 8);
 
-            // CalcValidMoves
+            // Test CalcValidMoves
             foreach (Position pos in CalcValidMoves(world.aurora.pos))
             {
                 Debug.Assert(DistanceBetween(pos, world.aurora.pos) == 1);
             }
 
-            // MoveCloserTo
+            // Test MoveCloserTo
             foreach (Position pos in MoveCloserTo(world.aurora.pos, world.trollKing.pos))
             {
                 Debug.Assert(DistanceBetween(pos, world.aurora.pos) == 1);
             }
 
-            // CharacterToCreature
+            // Test CharacterToCreature
             Debug.Assert(CharacterToCreature(world.aurora).attack == world.aurora.attack);
             Debug.Assert(CharacterToCreature(world.aurora).health == world.aurora.health);
             Debug.Assert(CharacterToCreature(world.aurora).maxHealth == world.aurora.maxHealth);
